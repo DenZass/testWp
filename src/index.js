0c31331f225d5b26dfefda7de1e979dd83364d8a
Daniil Zagorodnij <zagorodnijdaniil@yandex.ru>
2019-09-25 18:26:06 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import "./style.css";
+import "./mod/mod1";
+console.log('Hello');
+console.log(1 + '1');
+
+
---
+++ src/mod/mod1.js
@@ -0,0 +1,10 @@
+
+
+const div = document.querySelector('.paz');
+const divCs = document.createElement('div');
+
+console.log(div);
+
+div.textContent = 'Красавчик';
+divCs.classList.add('csss');
+console.log('priva');
